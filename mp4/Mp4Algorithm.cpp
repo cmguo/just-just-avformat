7b279e683934da95992f805789c47fdad8f422ab
isxxguo <isxxguo@pptv.com>
2012-10-15 06:43:43 +0000

    /ppbox/branches/1.2.0 /src/ppbox/avformat/mp4/Mp4Algorithm.cpp 不依赖开始的流字节数，中间可能会有更多字节
    git-svn-id: http://svn01/repositories/r01/ppbox/branches/1.2.0@5222 b3a1389e-2159-5c43-9038-463bf877d0d9

--- mp4/Mp4Algorithm.cpp
+++ mp4/Mp4Algorithm.cpp
@@ -17,15 +17,14 @@ namespace ppbox
             boost::system::error_code & ec)
         {
             std::ios::pos_type old_pos = is.tellg();
-            is.seekg(0, std::ios_base::end);
-            boost::uint64_t total_len = is.tellg();
+            assert(is);
             boost::uint64_t head_len = 0;
             while (true) {
-                if(total_len < head_len + 8) {
-                    head_len = head_len + 8;
-                    ec = ppbox::demux::error::file_stream_error;
-                    break;
-                }
+                //if(total_len < head_len + 8) {
+                //    head_len = head_len + 8;
+                //    ec = ppbox::demux::error::file_stream_error;
+                //    break;
+                //}
                 union {
                     boost::uint8_t c[8];
                     boost::uint32_t n[2];
@@ -33,7 +32,9 @@ namespace ppbox
                 is.seekg(head_len, std::ios_base::beg);
                 is.read(c, 8);
                 if (!is) {
-                    head_len = 0; 
+                    is.clear();
+                    assert(is);
+                    head_len += 8;
                     ec = ppbox::demux::error::file_stream_error;
                     break;
                 }
@@ -51,6 +52,7 @@ namespace ppbox
                 head_len += n[0];
             }
             is.seekg(old_pos, std::ios::beg);
+            assert(is);
             return head_len;
         }
 
