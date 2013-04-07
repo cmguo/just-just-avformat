66b775429959478e4196ef87a42f80d230d57e06
isxxguo <isxxguo@pptv.com>
2013-04-07 02:15:10 +0000

    /ppbox/branches/1.2.0 /src/ppbox 调整 ClassFactory 中的 Class 注册实现，修正包含顺序错误引起的注册不生效问题 emailto: box-dev
    git-svn-id: http://svn01/repositories/r01/ppbox/branches/1.2.0@6561 b3a1389e-2159-5c43-9038-463bf877d0d9

--- codec/CodecTypes.h
+++ codec/CodecTypes.h
@@ -3,7 +3,7 @@
 #ifndef _PPBOX_AVFORMAT_CODEC_CODEC_TYPES_H_
 #define _PPBOX_AVFORMAT_CODEC_CODEC_TYPES_H_
 
-#define PPBOX_ENABLE_REGISTER_CLASS
+#include <ppbox/common/ClassRegister.h>
 
 #include "ppbox/avformat/codec/avc/AvcCodec.h"
 #include "ppbox/avformat/codec/aac/AacCodec.h"
