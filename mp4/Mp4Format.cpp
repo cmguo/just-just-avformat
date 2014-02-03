dd058300aec38f3a61a1d8d2ea8eb8352758433c
isxxguo <isxxguo@pptv.com>
2014-02-03 07:22:32 +0000

    /ppbox/branches/1.2.0 /src/ppbox/demux/basic/mp4 使用自定义 MPEG Object Type 枚举 emailto: box-dev
    git-svn-id: http://svn01/repositories/r01/ppbox/branches/1.2.0@7984 b3a1389e-2159-5c43-9038-463bf877d0d9

--- mp4/Mp4Format.cpp
+++ mp4/Mp4Format.cpp
@@ -2,7 +2,7 @@
 
 #include "ppbox/avformat/Common.h"
 #include "ppbox/avformat/mp4/Mp4Format.h"
-#include <bento4/Core/Ap4SampleDescription.h>
+#include "ppbox/avformat/mp4/box/Mp4BoxEnum.h"
 
 #include <ppbox/avcodec/avc/AvcFormatType.h>
 #include <ppbox/avcodec/aac/AacFormatType.h>
@@ -14,17 +14,15 @@ namespace ppbox
     namespace avformat
     {
 
-        const AP4_MpegSampleDescription::OTI AP4_OTI_MPEG4_PART10_VISUAL         = 0x21; //AVC
-
         CodecInfo const Mp4Format::codecs_[] = {
-            {StreamType::VIDE,  AP4_OTI_MPEG4_VISUAL,           VideoSubType::MP4V, StreamFormatType::none,     0}, 
-            {StreamType::VIDE,  AP4_OTI_MPEG4_PART10_VISUAL,    VideoSubType::AVC1, AvcFormatType::packet,      0}, 
-            {StreamType::AUDI,  AP4_OTI_MPEG4_AUDIO,            AudioSubType::MP4A, AacFormatType::raw,         0}, 
-            {StreamType::AUDI,  AP4_OTI_MPEG2_AAC_AUDIO_MAIN,   AudioSubType::MP4A, AacFormatType::raw,         0}, 
-            {StreamType::AUDI,  AP4_OTI_MPEG2_AAC_AUDIO_LC,     AudioSubType::MP4A, AacFormatType::raw,         0}, 
-            {StreamType::AUDI,  AP4_OTI_MPEG2_AAC_AUDIO_SSRP,   AudioSubType::MP4A, AacFormatType::raw,         0}, 
-            {StreamType::AUDI,  AP4_OTI_MPEG2_PART3_AUDIO,      AudioSubType::MP2A, StreamFormatType::none,     0}, 
-            {StreamType::AUDI,  AP4_OTI_MPEG1_AUDIO,            AudioSubType::MP1A, StreamFormatType::none,     0}, 
+            {StreamType::VIDE,  MpegObjectType::MPEG4_VISUAL,           VideoSubType::MP4V, StreamFormatType::none,     0}, 
+            {StreamType::VIDE,  MpegObjectType::MPEG4_PART10_VISUAL,    VideoSubType::AVC1, AvcFormatType::packet,      0}, 
+            {StreamType::AUDI,  MpegObjectType::MPEG4_AUDIO,            AudioSubType::MP4A, AacFormatType::raw,         0}, 
+            {StreamType::AUDI,  MpegObjectType::MPEG2_AAC_AUDIO_MAIN,   AudioSubType::MP4A, AacFormatType::raw,         0}, 
+            {StreamType::AUDI,  MpegObjectType::MPEG2_AAC_AUDIO_LC,     AudioSubType::MP4A, AacFormatType::raw,         0}, 
+            {StreamType::AUDI,  MpegObjectType::MPEG2_AAC_AUDIO_SSRP,   AudioSubType::MP4A, AacFormatType::raw,         0}, 
+            {StreamType::AUDI,  MpegObjectType::MPEG2_PART3_AUDIO,      AudioSubType::MP2A, StreamFormatType::none,     0}, 
+            {StreamType::AUDI,  MpegObjectType::MPEG1_AUDIO,            AudioSubType::MP1A, StreamFormatType::none,     0}, 
         };
 
         Mp4Format::Mp4Format()
