984007f5f11b1f0e79487c118b50f212fb2577fc
isxxguo <isxxguo@pptv.com>
2014-02-06 11:22:49 +0000

    /ppbox/branches/1.2.0 /src/ppbox/dispatch/merge/MergeDispatcher.cpp 切换会话，重用MergeDispatcher，调用 Merger::reset emailto: box-dev
    git-svn-id: http://svn01/repositories/r01/ppbox/branches/1.2.0@8011 b3a1389e-2159-5c43-9038-463bf877d0d9

---
+++ mp4/lib/Mp4Error.h
@@ -0,0 +1,77 @@
+// Mp4Error.h
+
+#ifndef _PPBOX_AVFORMAT_MP4_LIB_MP4_ERROR_H_
+#define _PPBOX_AVFORMAT_MP4_LIB_MP4_ERROR_H_
+
+namespace ppbox
+{
+    namespace avformat
+    {
+
+        namespace mp4_error {
+
+            enum errors
+            {
+                invalid_mp4_head = 1, 
+                invalid_mp4_truck,
+            };
+
+            namespace detail {
+
+                class mp4_category
+                    : public boost::system::error_category
+                {
+                public:
+                    const char* name() const
+                    {
+                        return "mp4";
+                    }
+
+                    std::string message(int value) const
+                    {
+                        switch (value) 
+                        {
+                            case invalid_mp4_head:
+                                return "invalid mp4 head";
+                            case invalid_mp4_truck:
+                                return "invalid mp4 truck";
+                            default:
+                                return "mp4 other error";
+                        }
+                    }
+                };
+
+            } // namespace detail
+
+            inline const boost::system::error_category & get_category()
+            {
+                static detail::mp4_category instance;
+                return instance;
+            }
+
+            inline boost::system::error_code make_error_code(
+                errors e)
+            {
+                return boost::system::error_code(
+                    static_cast<int>(e), get_category());
+            }
+
+        } // namespace mp4_error
+
+    } // namespace live
+} // namespace ppbox
+
+namespace boost
+{
+    namespace system
+    {
+
+        template<>
+        struct is_error_code_enum<ppbox::avformat::mp4_error::errors>
+        {
+            BOOST_STATIC_CONSTANT(bool, value = true);
+        };
+    }
+}
+
+#endif // _PPBOX_AVFORMAT_MP4_LIB_MP4_ERROR_H_
