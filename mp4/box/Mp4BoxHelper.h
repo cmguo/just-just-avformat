1aa1f462ff2a29c9e49cb69b6a605b7bc5c6838d
isxxguo <isxxguo@pptv.com>
2014-09-03 13:19:09 +0800

    Mp4BoxHelper 打印未知 box
    Change-Id: I7e40e9179d4ac48920206a805eb52e3e915d7309

--- mp4/box/Mp4BoxHelper.h
+++ mp4/box/Mp4BoxHelper.h
@@ -8,6 +8,10 @@
 #include <ppbox/avbase/object/ObjectHelper.h>
 #include <ppbox/avbase/FourCC.h>
 
+#include <framework/logger/Logger.h>
+#include <framework/logger/StreamRecord.h>
+#include <framework/logger/DataRecord.h>
+
 namespace ppbox
 {
     namespace avformat
@@ -30,10 +34,18 @@ namespace ppbox
 
             ~Mp4BoxDataHelper()
             {
+                if (h_.object().empty()) {
+                    LOG_DEBUG("unknown box type: " << ctx_->path);
+                    Mp4Box::raw_data_t raw_data(h_.object().raw_data());
+                    LOG_DATA(framework::logger::Trace, ("raw data", raw_data.address(), raw_data.size()));
+                }
                 ctx_->data_ends.pop_back();
                 ctx_->stack.pop_back();
                 ctx_->path.erase(ctx_->path.size() - 5);
             }
+
+        private:
+            FRAMEWORK_LOGGER_DECLARE_MODULE_LEVEL("ppbox.avformat.Mp4BoxDataHelper", framework::logger::Debug);
 
         private:
             Mp4BoxContext * ctx_;
